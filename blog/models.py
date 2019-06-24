31cb0262436bc7114e94ce7d2cc68a62ef0a3a39
mhk1121992 <mhk1121992@gmail.com>
2019-06-25 00:35:29 +0600

    did the final touch,every link works and can see individual blog post

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,11 @@ class Blog(models.Model):
     text_body = models.TextField(max_length=20000)
     image = models.ImageField(upload_to='images/')
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.text_body[:100]
+
+    def pub_date_new(self):
+        return self.publish_date.strftime(' %b %e %Y')
